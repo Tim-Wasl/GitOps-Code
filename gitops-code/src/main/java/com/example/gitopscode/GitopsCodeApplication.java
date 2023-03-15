d5027a6d189483a429d4dd2790686cb8f85cb706
Wasl Tim <Tim.Wasl@adesso.de>
2023-03-15 13:49:16 +0100

    spring boot setup

---
+++ gitops-code/src/main/java/com/example/gitopscode/GitopsCodeApplication.java
@@ -0,0 +1,15 @@
+package com.example.gitopscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitopsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitopsCodeApplication.class, args);
+	}
+
+	
+
+}
---
+++ gitops-code/src/test/java/com/example/gitopscode/GitopsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitopscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitopsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
